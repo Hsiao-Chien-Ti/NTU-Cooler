35d9ae60d57855159673be59db6739400085b0c4
b09901042@ntu.edu.tw <kkyzl>
2023-01-01 05:46:45 +0800

    finish routing without re-rendering sidebar after login with bug while logout

--- frontend/src/containers/SignIn.js
+++ frontend/src/containers/SignIn.js
@@ -1,70 +1,66 @@
 import Title from "../components/Title";
 import LogIn from "../components/Login";
-import {useAll} from "./hooks/useAll";
-import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import { useAll } from "./hooks/useAll";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    width: 500px;
-    margin: auto;`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 500px;
+  margin: auto;
+`;
 const SignIn = () => {
-    const { user,setUser, displayStatus, signIn,loginData,getGrade,subject } = useAll();
-    const navigate = useNavigate();
-    const handleLogin = async(s) => {
-        const{studentID,passwd}=s
-        if (!studentID)
-        {
-            displayStatus({
-                type: "error",
-                msg: "Missing student ID",
-            });
-        }
-        else if(!passwd)
-        {
-            displayStatus({
-                type: "error",
-                msg: "Missing passwd",
-            });
-        }
-        else
-        {
-            await signIn({ variables: { studentID:studentID,passwd:passwd } })
-        }
+  const { user, setUser, displayStatus, signIn, loginData, getGrade, subject } =
+    useAll();
+  const navigate = useNavigate();
+  const handleLogin = async (s) => {
+    const { studentID, passwd } = s;
+    if (!studentID) {
+      displayStatus({
+        type: "error",
+        msg: "Missing student ID",
+      });
+    } else if (!passwd) {
+      displayStatus({
+        type: "error",
+        msg: "Missing passwd",
+      });
+    } else {
+      await signIn({ variables: { studentID: studentID, passwd: passwd } });
     }
-    useEffect(()=>{
-        if(user.login)
-            navigate('/homepage')
-    },[user])
-    // useEffect(()=>{
-    //     console.log(loginData)
-    //     if(loginData!=undefined)
-    //     {
-    //         setUser(loginData.login)
-    //         if(!loginData.login.login)
-    //         {
-    //             displayStatus({
-    //                 type: "error",
-    //                 msg: "Invalid student ID or password",
-    //                 duration:1
-    //             });   
-    //         }
-    //         else
-    //         {
-    //             if(user.login)
-    //                 navigate('/homepage')
-    //         }
-    //     }
-    // },[loginData])
-    return (
-        <Wrapper>
-            <Title />
-            <LogIn onLogin={handleLogin} />
-        </Wrapper>
-    );
-}
-export default SignIn
+  };
+  useEffect(() => {
+    if (user.login) navigate("/course/homepage");
+  }, [user]);
+  // useEffect(()=>{
+  //     console.log(loginData)
+  //     if(loginData!=undefined)
+  //     {
+  //         setUser(loginData.login)
+  //         if(!loginData.login.login)
+  //         {
+  //             displayStatus({
+  //                 type: "error",
+  //                 msg: "Invalid student ID or password",
+  //                 duration:1
+  //             });
+  //         }
+  //         else
+  //         {
+  //             if(user.login)
+  //                 navigate('/homepage')
+  //         }
+  //     }
+  // },[loginData])
+  return (
+    <Wrapper>
+      <Title />
+      <LogIn onLogin={handleLogin} />
+    </Wrapper>
+  );
+};
+export default SignIn;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,49 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
-    ApolloClient, InMemoryCache, ApolloProvider,
-    split, HttpLink
-} from '@apollo/client';
-import { getMainDefinition } from
-    '@apollo/client/utilities';
-import { GraphQLWsLink } from
-    '@apollo/client/link/subscriptions';
-import { createClient } from 'graphql-ws';
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  split,
+  HttpLink,
+} from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
 import { AllProvider } from "./containers/hooks/useAll";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 const httpLink = new HttpLink({
-    uri: 'http://localhost:4000/graphql'
+  uri: "http://localhost:4000/graphql",
 });
-const wsLink = new GraphQLWsLink(createClient({
-    url: 'ws://localhost:4000/graphql',
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4000/graphql",
     options: {
-        lazy: true,
+      lazy: true,
     },
-}));
+  })
+);
 const splitLink = split(
-    ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-            definition.kind === 'OperationDefinition' &&
-            definition.operation === 'subscription'
-        );
-    },
-    wsLink,
-    httpLink,
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
 );
 const client = new ApolloClient({
-    link: splitLink,
-    cache: new InMemoryCache(),
+  link: splitLink,
+  cache: new InMemoryCache(),
 });
-const root =
-    ReactDOM.createRoot(document.getElementById("root")
-    );
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <ApolloProvider client={client}>
-            <AllProvider><App /></AllProvider>
-        </ApolloProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <AllProvider>
+        <App />
+      </AllProvider>
+    </ApolloProvider>
+  </React.StrictMode>
 );
 reportWebVitals();
