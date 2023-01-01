d2d8851f7beacd1d7ceef57f38db9474568368e5
b09901042@ntu.edu.tw <kkyzl>
2023-01-01 14:26:27 +0800

    before rebase

--- backend/src/resolvers/Subscription.js
+++ backend/src/resolvers/Subscription.js
@@ -1,9 +1,9 @@
 const Subscription = {
   message: {
     subscribe: (parent, { to, courseID }, { pubsub }) => {
-      return pubsub.subscribe(`chatBox ${to}`);
+      return pubsub.subscribe(`chatBox ${to} in class ${courseID}`);
     },
   },
 };
 export { Subscription as default };
-// in class ${courseID}
+//
