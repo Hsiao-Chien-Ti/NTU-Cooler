f3962391ca0d09a198894d71c6f17fb9bd19656c
Hsiao-Chien-Ti <0410adi@gmail.com>
2023-01-05 19:39:40 +0800

    local final

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,6 @@ import { createServer } from "node:http";
 import { useServer } from "graphql-ws/lib/use/ws";
 import { WebSocketServer } from "ws";
 import * as fs from "fs";
-import express from 'express'
-import path from 'path';
-import cors from 'cors'
 // models
 import UserModel from "./models/user";
 import SyllabusModel from "./models/syllabus";
@@ -15,7 +12,6 @@ import FileModel from "./models/file";
 import ChatBoxModel from "./models/chatbox";
 import InfoModel from "./models/info";
 import HWModel from "./models/hw";
-import QuizModel from "./models/quiz";
 // graphql
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
@@ -41,79 +37,52 @@ const yoga = createYoga({
     InfoModel,
     HWModel,
     ChatBoxModel,
-    QuizModel,
     pubsub,
   },
   // graphqlEndpoint: '/',   // uncomment this to send the app to: 4000/ otherwise: 4000/graphql
-  // graphiql: {
-  //   subscriptionsProtocol: "WS",
-  // },
-  // graphqlEndpoint: "/graphql"
+  graphiql: {
+    subscriptionsProtocol: "WS",
+  },
 });
-// const httpServer = createServer(yoga);
 
-// const wsServer = new WebSocketServer({
-//   server: httpServer,
-//   path: yoga.graphqlEndpoint,
-// });
-// const server = express();
+const httpServer = createServer(yoga);
 
-// if (process.env.NODE_ENV === 'production') {
-//   const __dirname = path.resolve();
-//   console.log(express.static(path.join(__dirname, "../frontend", "build")));
-//   server.use(express.static(path.join(__dirname, "../frontend", "build")));
-//   server.get('*', (req, res) =>
-//     res.sendFile(path.join(__dirname, '../../frontend/build/index.html')))
-// }
-// else {
-//   server.use(cors());
-// }
-const server = express();
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: yoga.graphqlEndpoint,
+});
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  console.log(express.static(path.join(__dirname, "../frontend", "build")));
-  server.use(express.static(path.join(__dirname, "../frontend", "build")));
-  server.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
-}
-else {
-  server.use(cors());
-}
+useServer(
+  {
+    execute: (args) => args.rootValue.execute(args),
+    subscribe: (args) => args.rootValue.subscribe(args),
+    onSubscribe: async (ctx, msg) => {
+      const { schema, execute, subscribe, contextFactory, parse, validate } =
+        yoga.getEnveloped({
+          ...ctx,
+          req: ctx.extra.request,
+          socket: ctx.extra.socket,
+          params: msg.payload,
+        });
 
-server.use('/graphql', yoga);
+      const args = {
+        schema,
+        operationName: msg.payload.operationName,
+        document: parse(msg.payload.query),
+        variableValues: msg.payload.variables,
+        contextValue: await contextFactory(),
+        rootValue: {
+          execute,
+          subscribe,
+        },
+      };
 
-// useServer(
-//   {
-//     execute: (args) => args.rootValue.execute(args),
-//     subscribe: (args) => args.rootValue.subscribe(args),
-//     onSubscribe: async (ctx, msg) => {
-//       const { schema, execute, subscribe, contextFactory, parse, validate } =
-//         yoga.getEnveloped({
-//           ...ctx,
-//           req: ctx.extra.request,
-//           socket: ctx.extra.socket,
-//           params: msg.payload,
-//         });
+      const errors = validate(args.schema, args.document);
+      if (errors.length) return errors;
+      return args;
+    },
+  },
+  wsServer
+);
 
-//       const args = {
-//         schema,
-//         operationName: msg.payload.operationName,
-//         document: parse(msg.payload.query),
-//         variableValues: msg.payload.variables,
-//         contextValue: await contextFactory(),
-//         rootValue: {
-//           execute,
-//           subscribe,
-//         },
-//       };
-
-//       const errors = validate(args.schema, args.document);
-//       if (errors.length) return errors;
-//       return args;
-//     },
-//   },
-//   wsServer
-// );
-
-export default server;
+export default httpServer;
