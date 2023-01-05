096b131a5bf96b097094cf1b6e6dda994bd09313
Hsiao-Chien-Ti <0410adi@gmail.com>
2023-01-05 19:36:46 +0800

    deploy final add data

--- backend/src/upload.js
+++ backend/src/upload.js
@@ -60,12 +60,123 @@ const testUser = [
     groupNum: 7,
     isTeacher: false,
   },
+  {
+    name: "Teacher",
+    studentID: "teacher",
+    chatbox: [
+      { name: "quiz 1", courseID: "EE1234", showName: "quiz 1", type: true },
+    ],
+    passwd: bcrypt.hashSync("12345678", 14),
+    groupNum: 0,
+    isTeacher: true,
+  },
+  {
+    name: "Student",
+    studentID: "student",
+    chatbox: [
+      {
+        name: "NI_MEN_DA_ER",
+        coursID: "EE1234",
+        showName: "NI_MEN_DA_ER",
+        type: false,
+      },
+      {
+        name: "quiz 1",
+        courseID: "EE1234",
+        showName: "quiz 1",
+        type: true
+      },
+    ],
+    passwd: bcrypt.hashSync("87654321", 14),
+    groupNum: 3,
+    isTeacher: false,
+  },
+  {
+    name: "qian",
+    studentID: "pbwhere",
+    passwd: "12345678",
+    groupNum: 3,
+    login: false,
+    isTeacher: false,
+    chatbox: [
+      {
+        name: "NI_MEN_DA_ER",
+        coursID: "EE1234",
+        showName: "NI_MEN_DA_ER",
+        type: false,
+      },
+    ]
+  },
+  {
+    name: "adi",
+    studentID: "aaaaadi",
+    passwd: "12345678",
+    groupNum: 2,
+    login: false,
+    isTeacher: false,
+    chatbox: [
+      {
+        name: "NI_MEN_DA_ER",
+        coursID: "EE1234",
+        showName: "NI_MEN_DA_ER",
+        type: false,
+      },
+    ]
+  },
+  {
+    name: "angel",
+    studentID: "hsiaYuTien",
+    passwd: "12345678",
+    groupNum: 6,
+    login: false,
+    isTeacher: false,
+    chatbox: [
+      {
+        name: "NI_MEN_DA_ER",
+        coursID: "EE1234",
+        showName: "NI_MEN_DA_ER",
+        type: false,
+      },
+    ]
+  },
+  {
+    name: "shootingStar",
+    studentID: "heartUniverse113",
+    passwd: "12345678",
+    groupNum: 2,
+    login: false,
+    isTeacher: false,
+    chatbox: [
+      {
+        name: "NI_MEN_DA_ER",
+        coursID: "EE1234",
+        showName: "NI_MEN_DA_ER",
+        type: false,
+      },
+    ]
+  },
+  {
+    name: "frenchfries",
+    studentID: "potatolove23",
+    passwd: "12345678",
+    groupNum: 4,
+    login: false,
+    isTeacher: false,
+    chatbox: [
+      {
+        name: "NI_MEN_DA_ER",
+        coursID: "EE1234",
+        showName: "NI_MEN_DA_ER",
+        type: false,
+      },
+    ]
+  },
 ];
 
 const testChat = [
   {
     name: "quiz 1",
-    notAccess: ["b09901042"],
+    notAccess: ["b09901042", "student"],
     messages: [
       {
         sender: { studentID: "t", name: "t", groupNum: 0 },
@@ -100,7 +211,7 @@ const testChat = [
         hidden: true,
       },
     ],
-    participants: ["KKK", "q", "b09901042", "t"],
+    participants: ["KKK", "q", "b09901042", "t", "student"],
     type: true,
     courseID: "EE1234",
     pinMsg: 0,
@@ -140,6 +251,106 @@ const testChat = [
     courseID: "EE1234",
     pinMsg: -1,
   },
+  {
+    type: false,
+    courseID: "EE1234",
+    name: "NI_MEN_DA_ER",
+    notAccess: [],
+    messages: [
+      {
+        sender: {
+          studentID: "student",
+          name: "Student",
+          groupNum: -1,
+        },
+        body: "hi 大家好呀",
+        hidden: false,
+      },
+      {
+        sender: {
+          studentID: "aaaaadi",
+          name: "adi",
+          groupNum: -1,
+        },
+        body: "要不要打球",
+        hidden: false,
+      },
+      {
+        sender: {
+          studentID: "hsiaYuTien",
+          name: "angel",
+          groupNum: -1,
+        },
+        body: "走",
+        hidden: false,
+      },
+      {
+        sender: {
+          studentID: "heartUniverse113",
+          name: "shootingStar",
+          groupNum: -1,
+        },
+        body: "明天好像會下雨",
+        hidden: false,
+      },
+      {
+        sender: {
+          studentID: "aaaaadi",
+          name: "adi",
+          groupNum: -1,
+        },
+        body: "還是去打保齡球",
+        hidden: false,
+      },
+      {
+        sender: {
+          studentID: "KKK",
+          name: "KKK",
+          groupNum: -1,
+        },
+        body: "加加",
+        hidden: false,
+      },
+      {
+        sender: {
+          studentID: "aaaaadi",
+          name: "adi",
+          groupNum: -1,
+        },
+        body: "明天早上十點劍潭站集合",
+        hidden: false,
+      },
+      {
+        sender: {
+          studentID: "potatolove23",
+          name: "frenchfries",
+          groupNum: -1,
+        },
+        body: "要等我喔",
+        hidden: false,
+      },
+      {
+        sender: {
+          studentID: "pbwhere",
+          name: "qian",
+          groupNum: -1,
+        },
+        body: "你會遲到",
+        hidden: false,
+      },
+      {
+        sender: {
+          studentID: "potatolove23",
+          name: "frenchfries",
+          groupNum: -1,
+        },
+        body: "這樣子講話的=(",
+        hidden: false,
+      },
+    ],
+    pinMsg: 6,
+    participants: ["student", "pbwhere", "KKK", "potatolove23", "heartUniverse113", "hsiaYuTien", "aaaaadi"]
+  },
 ];
 
 const testQuiz = [
@@ -165,62 +376,106 @@ const testInfo = [
 const testSyllabus = [
   {
     weekNum: "1",
-    outline: "No class for week 1",
+    outline: "Introduction to the structure of network",
   },
   {
     weekNum: "2",
-    outline: "Field trip",
+    outline: "network core",
+  },
+  {
+    weekNum: "3",
+    outline: "packet loss in network",
+  },
+  {
+    weekNum: "4",
+    outline: "TCP and UDP",
+  },
+  {
+    weekNum: "5",
+    outline: "HTTP and DNS",
+  },
+  {
+    weekNum: "6",
+    outline: "IP datagram",
   },
 ];
 const testFile = [
   {
     type: "weekNum",
     info: "1",
-    fileName: "CPBL",
-    fileLink: "https://www.cpbl.com.tw/",
+    fileName: "What is network layer?",
+    fileLink: "https://www.cloudflare.com/zh-tw/learning/network-layer/what-is-the-network-layer/",
     linkType: false,
     studentID: "",
   },
   {
     type: "weekNum",
-    info: "2",
-    fileName: "Taipei children's amusement park",
-    fileLink: "https://www.tcap.taipei/",
+    info: "6",
+    fileName: "What is my ip",
+    fileLink: "https://www.whatismyip.com.tw/tw/",
     linkType: false,
     studentID: "",
   },
   {
     type: "tHW",
-    info: "hw1",
-    fileName: "Leetcode",
-    fileLink: "https://leetcode.com/problemset/all/",
+    info: "PA1",
+    fileName: "Golang",
+    fileLink: "https://go.dev/",
     linkType: false,
     studentID: "",
+  },
+  {
+    type: "tHW",
+    info: "PA2",
+    fileName: "goroutine",
+    fileLink: "https://go.dev/tour/concurrency/1",
+    linkType: false,
   },
 ];
 const testHW = [
   {
     title: "hw1",
-    deadline: "2023-01-06 01:01",
+    deadline: "2022-10-06 23:59",
     description: "This is your first homework",
   },
   {
     title: "hw2",
-    deadline: "2023-01-07 01:01",
+    deadline: "2023-11-07 08:00",
     description: "This is your second homework",
+  },
+  {
+    title: "PA1",
+    deadline: "2022-12-04 10:20",
+    description: "First meet with Golang",
+  },
+  {
+    title: "hw3",
+    deadline: "2022-12-17 23:59",
+    description: "DV and LS practice",
+  },
+  {
+    title: "PA2",
+    deadline: "2023-01-04 10:20",
+    description: "Goroutine",
   },
 ];
 const testAnnouncement = [
   {
-    time: "2023-01-01 01:01",
+    time: "2022-12-31 13:16",
     title: "Happy new year",
-    content: "Firework!!!!!",
+    content: "Enjoy the firework show!!!!! BUT Don't forget to prepare for the upcoming exam!!!",
   },
   {
     time: "2023-01-05 01:01",
-    title: "Good Morning",
-    content: "I am hungry",
+    title: "Postpone of Exam 3",
+    content: "Due to our delay on the course syllabus, the exam will be postponed for two weeks",
   },
+  {
+    time: "2023-02-05 12:00",
+    title: "Lantern Festival",
+    content: "Don't forget to eat yuanxiao!!!",
+  },
+
 ];
 const testGrade = [
   {
