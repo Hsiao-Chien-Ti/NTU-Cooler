03a36cea19e2a9132ca7e495fa15fb5f9fd98d9c
盧音孜 <75139223+kkyzl@users.noreply.github.com>
2023-01-05 09:55:32 +0800

    nothing

--- frontend/src/containers/hooks/useAll.js
+++ frontend/src/containers/hooks/useAll.js
@@ -26,7 +26,7 @@ const LOCALSTORAGE_KEY = "save-me";
 const savedMe = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
 const AllContext = createContext({
-  user: { name: "", passwd: "", studentID: "", login: false },
+  user: { name: "", passwd: "", studentID: "", login: false, groupNum: -1 },
   attendants: [],
   allStudents: [],
   courseID: "",
