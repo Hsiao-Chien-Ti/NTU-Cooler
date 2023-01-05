119c8df879f2c30ce50927412cdb79d125458b62
Hsiao-Chien-Ti <0410adi@gmail.com>
2023-01-05 10:27:49 +0800

    deploy 6th same as ref

--- backend/src/server.js
+++ backend/src/server.js
@@ -45,46 +45,59 @@ const yoga = createYoga({
   graphiql: {
     subscriptionsProtocol: "WS",
   },
+  graphqlEndpoint: "/graphql"
 });
+const server = express();
 
-const httpServer = createServer(yoga);
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  console.log(express.static(path.join(__dirname, "../frontend", "build")));
+  server.use(express.static(path.join(__dirname, "../frontend", "build")));
+}
+else {
+  server.use(cors());
+}
 
-const wsServer = new WebSocketServer({
-  server: httpServer,
-  path: yoga.graphqlEndpoint,
-});
+server.use('/graphql', yoga);
 
-useServer(
-  {
-    execute: (args) => args.rootValue.execute(args),
-    subscribe: (args) => args.rootValue.subscribe(args),
-    onSubscribe: async (ctx, msg) => {
-      const { schema, execute, subscribe, contextFactory, parse, validate } =
-        yoga.getEnveloped({
-          ...ctx,
-          req: ctx.extra.request,
-          socket: ctx.extra.socket,
-          params: msg.payload,
-        });
+// const httpServer = createServer(yoga);
 
-      const args = {
-        schema,
-        operationName: msg.payload.operationName,
-        document: parse(msg.payload.query),
-        variableValues: msg.payload.variables,
-        contextValue: await contextFactory(),
-        rootValue: {
-          execute,
-          subscribe,
-        },
-      };
+// const wsServer = new WebSocketServer({
+//   server: httpServer,
+//   path: '/subscriptions',
+// });
 
-      const errors = validate(args.schema, args.document);
-      if (errors.length) return errors;
-      return args;
-    },
-  },
-  wsServer
-);
+// useServer(
+//   {
+//     execute: (args) => args.rootValue.execute(args),
+//     subscribe: (args) => args.rootValue.subscribe(args),
+//     onSubscribe: async (ctx, msg) => {
+//       const { schema, execute, subscribe, contextFactory, parse, validate } =
+//         yoga.getEnveloped({
+//           ...ctx,
+//           req: ctx.extra.request,
+//           socket: ctx.extra.socket,
+//           params: msg.payload,
+//         });
+
+//       const args = {
+//         schema,
+//         operationName: msg.payload.operationName,
+//         document: parse(msg.payload.query),
+//         variableValues: msg.payload.variables,
+//         contextValue: await contextFactory(),
+//         rootValue: {
+//           execute,
+//           subscribe,
+//         },
+//       };
+
+//       const errors = validate(args.schema, args.document);
+//       if (errors.length) return errors;
+//       return args;
+//     },
+//   },
+//   wsServer
+// );
 
 export default httpServer;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,38 +15,48 @@ import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 import { ChatProvider } from "./containers/hooks/useChat";
 import { QuizProvider } from "./containers/hooks/useQuiz";
-const httpLink = new HttpLink({
-  uri:   process.env.NODE_ENV === "production"
-  ? "https://ntu-cooler.onrender.com/graphql"
-  : "http://localhost:4000/graphql"
-,
-});
-const wsLink = new GraphQLWsLink(
-  createClient({
-    url:   process.env.NODE_ENV === "production"
-    ? "wss://ntu-cooler.onrender.com/graphql"
-    : "ws://localhost:4000/graphql",
-    options: {
-      lazy: true,
-    },
-  })
-);
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
-    );
-  },
-  wsLink,
-  httpLink
-);
-const client = new ApolloClient({
-  link: splitLink,
-  cache: new InMemoryCache(),
-});
+// const httpLink = new HttpLink({
+//   uri:   process.env.NODE_ENV === "production"
+//   ? "https://ntu-cooler.onrender.com/graphql"
+//   : "http://localhost:4000/graphql"
+// ,
+// });
+// const wsLink = new GraphQLWsLink(
+//   createClient({
+//     url:   process.env.NODE_ENV === "production"
+//     ? "wss://ntu-cooler.onrender.com/subscriptions"
+//     : "ws://localhost:4000/subscriptions",
+//     options: {
+//       lazy: true,
+//     },
+//   })
+// );
+// const splitLink = split(
+//   ({ query }) => {
+//     const definition = getMainDefinition(query);
+//     return (
+//       definition.kind === "OperationDefinition" &&
+//       definition.operation === "subscription"
+//     );
+//   },
+//   wsLink,
+//   httpLink
+// );
+// const client = new ApolloClient({
+//   link: splitLink,
+//   cache: new InMemoryCache(),
+// });
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const { YogaLink } = require("@graphql-yoga/apollo-link");
+
+const client = new ApolloClient({
+  link: new YogaLink({
+    endpoint: process.env.NODE_ENV === 'production' 
+      ? '/graphql'
+      : 'http://localhost:4000/graphql'
+  }),
+  cache: new InMemoryCache(),
+})
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
